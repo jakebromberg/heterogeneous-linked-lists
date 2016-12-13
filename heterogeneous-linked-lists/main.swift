15d56e4abfd09dfb19e42a4295c965c5719b81e7
Jake Bromberg <jakebromberg@nestlabs.com>
2016-12-12 21:55:01 -0800

    Adding >>> operator to make list declaration easier

--- heterogeneous-linked-lists/main.swift
+++ heterogeneous-linked-lists/main.swift
@@ -15,12 +15,35 @@ struct Node<ValueType, NextNodeType: NodeType> : NodeType {
 let a = TerminatingNode(value: 123)
 var b = Node(value: 456, next: a)
 var c = Node(value: "asdf", next: b)
-var d = Node(value: 123, next: c)
 
-assert(d.value == 123)
-assert(d.next.value == "asdf")
-assert(d.next.next.value == 456)
-assert(d.next.next.next.value == 123)
-assert(d.value == a.value)
-assert(type(of:d.value) == Int.self)
-assert(type(of:d.next.value) == String.self)
+assert(c.value == "asdf")
+assert(c.next.value == 456)
+assert(c.next.next.value == 123)
+assert(type(of:c.next.value) != type(of:c.value))
+
+extension NodeType {
+    func prepend<U>(value: U) -> Node<U, Self> {
+        return Node(value: value, next: self)
+    }
+}
+
+precedencegroup ListInfixPrecedence {
+    associativity: right
+}
+
+infix operator >>> : ListInfixPrecedence
+
+func >>><T, U: NodeType>(lhs: T, rhs: U) -> Node<T, U> {
+    return Node(value: lhs, next: rhs)
+}
+
+prefix operator >>>
+
+func >>><T, U>(lhs: T, rhs: U) -> Node<T, TerminatingNode<U>> {
+    return TerminatingNode(value: rhs).prepend(value: lhs)
+}
+
+let list = "asdf" >>> 456 >>> 123
+assert(list.value == c.value)
+assert(list.next.value == c.next.value)
+assert(list.next.next.value == c.next.next.value)
